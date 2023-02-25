97e4b31aeb0b3b427bdb1fc19c6c3897e5ff48de
Nilsonlinux <nilsonlinux@gmail.com>
2023-02-25 11:04:46 -0300

    Add files via upload

--- comandos/add_printer.sh
+++ comandos/add_printer.sh
@@ -13,13 +13,13 @@
     NOME=
     IP=
     ###############################################################################################
-            yad --form --borders=10 --center --width=550                                          \
+      FORMULARY=$(GTK_THEME="$THEME" yad --form --borders=10 --center --width=550      \
           --window-icon="gtk-execute"  --image="printer-printing" --item-separator=","          \
           --title "$NOME_PROG - VERSÃO $ver"                                                      \
           --form                                                                                  \
           --field="N O M E  S E T O R" $NOME ""                                                   \
           --field="I P  D A  I M P R E S O R A" $IP ""                                            \
-          --button="IMPRESSORAS":$DIR/printer.sh --button="CANCELAR":1 --button="INSTALAR":0)
+          --button="CANCELAR":1 --button="INSTALAR":0)
       [ $? != 0 ] && exit
       NOME=$(echo $FORMULARY      | awk -F '|' '{ print $1 }')
       IP=$(echo $FORMULARY     | awk -F '|' '{ print $2 }')
