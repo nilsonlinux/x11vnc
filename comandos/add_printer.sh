626e42cea3445236f8e7baafe63c982e6ac5efc7
Nilsonlinux <nilsonlinux@gmail.com>
2023-02-25 16:25:22 -0300

    Update add_printer.sh

--- comandos/add_printer.sh
+++ comandos/add_printer.sh
@@ -29,9 +29,7 @@
       break
       done
 #########################################################################
-sudo lpadmin -p ${SETOR} -E -v socket://${IP}
-sudo cp $HOME/x11vnc/impressora.ppd /etc/cups/&{SETOR}.ppd
-sudo cp x11vnc/impressora.ppd /etc/cups/&{SETOR}.ppd
+sudo lpadmin -p ${SETOR} -E -v socket://${IP} && sudo cp $HOME/x11vnc/impressora.ppd /etc/cups/ppd/&{SETOR}.ppd
 #rm -rf $HOME/x11vnc
 ##########################################################################
 xdg-open http://localhost:631/printers/${SETOR}
