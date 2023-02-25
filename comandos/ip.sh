e6bc96e67089cb42a2fe7ba5eac3105f3c64c06b
Nilsonlinux <nilsonlinux@gmail.com>
2023-02-25 09:34:37 -0300

    Add files via upload

--- comandos/ip.sh
+++ comandos/ip.sh
@@ -5,7 +5,7 @@
   IP=$(hostname -I)
   ####################
 yad --form --title="Informação!" --height=300 --width=400 \
---center --image=/home/Nilsonlinux/.icons/net.svg \
+--center --image=network-wired \
 --borders=20 --button="Sair:0"  \
 --text="<span font_weight='bold' font='10' color='#FFBF00'>  O IP DESTA MÁQUINA É</span>
 <span font='25' color='#6DFB0E'>${IP} </span>" \
