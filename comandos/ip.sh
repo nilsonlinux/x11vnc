930b607d14ccf4d39893b69e67b4ea407e755b49
Nilsonlinux <nilsonlinux@gmail.com>
2023-02-25 11:18:30 -0300

    Add files via upload

--- comandos/ip.sh
+++ comandos/ip.sh
@@ -2,10 +2,11 @@
   #####################################################################################
   #### SUPORTE REGIONAL - PE/PB/AL/RN
   #####################################################################################
+  THEME="Mint-Y-Dark-Blue"
   IP=$(hostname -I)
   ####################
-yad --form --title="Informação!" --height=300 --width=400 \
---center --image=network-wired \
+  GTK_THEME="$THEME" yad --form --title="Informação!" --height=300 --width=400 \
+--center --image=/home/Nilsonlinux/.icons/net.svg \
 --borders=20 --button="Sair:0"  \
 --text="<span font_weight='bold' font='10' color='#FFBF00'>  O IP DESTA MÁQUINA É</span>
 <span font='25' color='#6DFB0E'>${IP} </span>" \
