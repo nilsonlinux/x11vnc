9f9ddb2af788ec35eb7512b1b7bf1262f81b07db
Nilsonlinux <nilsonlinux@gmail.com>
2023-02-27 15:01:37 -0300

    Update add_printer.sh

--- comandos/add_printer.sh
+++ comandos/add_printer.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
   #####################################################################################
-  #### SUPORTE REGIONA
-  #### Nilsonlinux
+  #### SUPORTE REGIONAL - PE/PB/AL/RN
   #####################################################################################
-# #####################################################################################
-  NOME_PROG="ADICIONAR IMPRESSORA"
-  THEME="Mint-Y-Dark-Blue"
-  #/usr/share/themes/Mint-Y-Dark-Blue
-  #####################################################################################
-  #############################   VERIFICAÇÃO DE CONEXÃO   ############################
-  #####################################################################################
-    while true
-    do
-    SETOR=
-    IP=
-    ###############################################################################################
-      FORMULARY=$(GTK_THEME="$THEME"  yad --form --borders=10                                     \
-      --borders=10 --center --width=550                                                           \
-          --window-icon="gtk-execute"  --image="printer-printing" --item-separator=","            \
-          --title "$NOME_PROG"                                                                    \
-          --form                                                                                  \
-          --field="S E T O R" ${SETOR} ""                                                         \
-          --field="I P" ${IP} ""                                                                  \
-          --button="CANCELAR":1 --button="INSTALAR":0)
-      [ $? != 0 ] && exit
-      SETOR=$(echo $FORMULARY  | awk -F '|' '{ print $1 }')
-      IP=$(echo $FORMULARY     | awk -F '|' '{ print $2 }')
-      break
-      done
-#########################################################################
-sudo lpadmin -p ${SETOR} -E -v socket://${IP}
-sudo cp $HOME/x11vnc/impressora.ppd /etc/cups/ppd/${SETOR}.ppd
-sudo service cups restart && sudo service cups start
-##########################################################################
-xdg-open http://localhost:631/printers/${SETOR}
+a='\033[1;33m'     # Amarelo
+p='\033[0;35m'     # Purple
+v="\033[0;31m"     #vermelho
+vr="\033[01;32m"   #Verde
+br="\033[0;37m"    #Branco
+v1="\033[0;32m"    #Verde escuro
+v2="\033[1;32m"    #verde claro
+end='\033[0m'
+##############################
+logo () {
+  clear
+echo -e "${v1} __     ___   _  ____    ______ ____  _   _ 
+\ \   / / \ | |/ ___|  / / ___/ ___|| | | |
+ \ \ / /|  \| | |     / /\___ \___ \| |_| |
+  \ V / | |\  | |___ / /  ___) |__) |  _  |
+   \_/  |_| \_|\____/_/  |____/____/|_| |_|
+   ${p}By: Nilsonlinux
+   ${v1}Instalação e configuração.
+   VNC Server e SSH + Atalhos. Para Linux Mint ${end}"
+ }
+##############################
+logo
+echo -e " ${c}ADICIONAR IMPRESSORA.${end}"
+echo -e "DIGITE ${c}A${end} ${br}IDENTIFICAÇÃO DA IMPRESSORA. (EX: RH, FATURAMENTO) ${end}"
+read -p "IDENTIFICAÇÃO IMPRESSORA: " $read NOME
+clear
+##########
+logo
+echo -e " ${c}ADICIONAR IMPRESSORA.${end}"
+echo -e "IDENTIFICAÇÃO DA IMPRESSORA: ${NOME}${end}"
+read -p "DIGITE O IP DA IMPRESSORA: " $read IP
+echo -e "${c}---------------------------------------------------${end}"
+echo -e "$vr======================================== $end"
+echo -e "$v         DIGITE A SENHA PARA CONTINUAR.  $end "
+echo -e "$vr======================================== $end"
+sudo lpadmin -p ${NOME} -E -v socket://${IP};
+sudo cp $HOME/x11vnc/impressora.ppd /etc/cups/ppd/${NOME}.ppd
+rm -rf $HOME/x11vnc
+
+
